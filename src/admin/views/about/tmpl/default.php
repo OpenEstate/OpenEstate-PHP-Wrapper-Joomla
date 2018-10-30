d93e32425a5d9819625c90ef062bf96cfc352408
Andreas Rudolph <andy@openindex.de>
2018-10-30 01:38:59 +0100

    switched license to GPL v2 or later

--- src/admin/views/about/tmpl/default.php
+++ src/admin/views/about/tmpl/default.php
@@ -3,17 +3,19 @@
  * A Joomla module for the OpenEstate-PHP-Export
  * Copyright (C) 2010-2018 OpenEstate.org
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 // no direct access
@@ -36,9 +38,14 @@ $mainContainerClass = (!empty($this->sidebar)) ? 'span10' : '';
     <h2><?php echo JText::_('COM_OPENESTATE_ABOUT_TITLE'); ?></h2>
     <p><?php echo JText::sprintf('COM_OPENESTATE_ABOUT_INFO', 'http://openestate.org'); ?></p>
     <ul>
-        <li><?php echo JText::_('COM_OPENESTATE_ABOUT_LICENSE'); ?>: <a
-                    href="./components/com_openestate/gpl-3.0-standalone.html" target="_blank">GPL3</a></li>
-        <li><?php echo JText::_('COM_OPENESTATE_ABOUT_AUTHORS'); ?>: Andreas Rudolph &amp; Walter Wagner</li>
+        <li>
+            <?php echo JText::_('COM_OPENESTATE_ABOUT_LICENSE'); ?>:
+            <a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPL2 (or later)</a>
+        </li>
+        <li>
+            <?php echo JText::_('COM_OPENESTATE_ABOUT_AUTHORS'); ?>:
+            Andreas Rudolph &amp; Walter Wagner
+        </li>
     </ul>
     <h3><?php echo JText::_('COM_OPENESTATE_ABOUT_SUPPORTUS'); ?></h3>
     <p><?php echo JText::_('COM_OPENESTATE_ABOUT_SUPPORTUS_TEXT'); ?></p>
